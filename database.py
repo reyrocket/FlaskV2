d61c91e438cda934eaa2e5e57129a5c8237f078e
reyrocket <72682574+reyrocket@users.noreply.github.com>
2023-05-15 08:30:33 +0000

    eeeeeeeeeeeee

---
+++ database.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine
+
+engine = create_engine("dysql+pymysql://bluxus2m49hggxo3p351:pscale_pw_VJBJwupClDnpYCSYjzeEPabtIPSYfWpbqW0o0cZqWhk@aws.connect.psdb.cloud/ flaskv2?charset=utf8mb4" )
