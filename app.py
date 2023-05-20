4e7038ce960b05eb7a2b6a02b75586aeec35c9ae
reyrocket <reybairagya@gmail>
2023-05-20 13:35:37 +0530

    Stuff added

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 def hello_jovian():
     jobs = load_data()
     return render_template('home.html',
-                           jobs=jobs,
-                           company_name='Jovian')
+                           jobs=jobs)
 
 
 @app.route("/api/jobs")
@@ -21,7 +20,11 @@ def list_jobs():
 @app.route("/jobs/<id>")
 def display(id):
     job = ljfdb(id)
-    return job
+    if not job:
+        return "Not Found", 404
+
+    return render_template('jobpage.html',
+                           job=job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,9 +1,5 @@
 from creds import USERNAME, PASSWORD
 from sqlalchemy import create_engine, text
-from dotenv import load_dotenv
-# from sqlalchemy import exc
-# from sqlalchemy import event
-# from sqlalchemy import select
 
 conn = f'mysql+pymysql://{USERNAME}:{PASSWORD}@aws.connect.psdb.cloud/flaskv2'
 some_engine = create_engine(conn, echo_pool=True, pool_size=7200, pool_recycle=3600, connect_args={
@@ -11,26 +7,6 @@ some_engine = create_engine(conn, echo_pool=True, pool_size=7200, pool_recycle=3
         "ssl_ca": "/etc/ssl/cert.pem"
     }
 })
-
-
-def configure():
-    load_dotenv()
-
-
-configure()
-
-
-# @event.listens_for(some_engine, "engine_connect")
-# def ping_connection(connection, branch):
-#     if branch:
-#         return
-#     try:
-#         connection.scalar(select(1))
-#     except exc.DBAPIError as err:
-#         if err.connection_invalidated:
-#             connection.scalar(select(1))
-#         else:
-#             raise
 
 
 def load_data():
@@ -48,12 +24,12 @@ def ljfdb(id):
     with some_engine.connect() as connection:
         # noinspection PyArgumentList,SqlInjection
         result = connection.execute(
-            text
-            (f"select * from jobs where id = :val"),
-            val=id)
-
-    rows = result.all()
-    if len(rows) == 0:
-        return None
-    else:
-        return dict(rows[0])
+            text("SELECT * FROM jobs WHERE id = :val"),
+            {"val": id}
+        )
+        rows = result.all()
+        for idd, title, location, salary, currency, responsibilities, requirements in rows:
+            if len(rows) == 0:
+                return None
+            else:
+                return dict(id=idd, title=title, location=location, salary=salary, currency=currency, responsibilities=responsibilities, requirements=requirements)
